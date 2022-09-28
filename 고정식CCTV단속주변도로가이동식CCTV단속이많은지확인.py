63541f30ecd33ca4110503d018f7be4b62fe0f04
Hyunjae Lee <97109063+LeeYunseol@users.noreply.github.com>
2022-09-29 00:41:03 +0900

    Add files via upload

--- 고정식CCTV단속주변도로가이동식CCTV단속이많은지확인.py
+++ 고정식CCTV단속주변도로가이동식CCTV단속이많은지확인.py
@@ -2,6 +2,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from tqdm import tqdm
 #%%
 # 한글 폰트 사용을 위해서 세팅
@@ -23,20 +24,30 @@ temp = temp_data['단속된 시간'].value_counts()
 ===============================================================================
 '''
 # 먼저 덕진구에 고정식 CCTV 단속이 많은 곳 확인하기
-data_dukjin = data.loc[(data['단속구분'] == "고정식CCTV") & (data['구청구분'] == "덕진구"), :]
+data_dukjin = data.loc[(data['단속구분'] == "고정식CCTV") & (data['구청구분'] == "덕진구") & (data['단속된 시간'] == 10), :]
 data_dukjin_place = data_dukjin['단속장소명'].value_counts()
 #%%
 # 주변 도로 확인
-data_cctv_move = data.loc[(data['단속구분'] == "이동식CCTV") & (data['구청구분'] == "덕진구"), :]
+data_cctv_move = data.loc[(data['단속구분'] == "이동식CCTV") & (data['구청구분'] == "덕진구") & (data['단속된 시간'] == 10), :]
 data_cctv_move_place = data_cctv_move['단속장소명'].value_counts()
-
+#%%
+# 박스플롯
+plt.boxplot(data_cctv_move_place)
+plt.show()
+#%%
+# 히스토그램
+sns.distplot(data_cctv_move_place)
+plt.show()
+#%%
+print(sum(data_cctv_move_place[0:19]))
+print(sum(data_cctv_move_place[0:19]) / sum(data_cctv_move_place) * 100)
 #%%
 # 덕진구 고정식 cctv 단속된 구간의 중위수와 4 사분위 비교하기
 q3 = data_dukjin_place.quantile(0.5) # 84.5 => 파인트리몰
-q4 = data_dukjin_place.quantile(0.75) # 515.75 => 롯데마트 주차장 입구 or 모래내시장 주차장입구
+q4 = data_dukjin_place.quantile(0.8) # 515.75 => 롯데마트 주차장 입구 or 모래내시장 주차장입구
 num = 0
 for i in list(list(data_cctv_move_place.index)) :
-    if '송천동 2가 붓내3길' in i :
+    if '온고올로' in i :
         print(num)
     num += 1
 print(data_cctv_move_place.iloc[38])
